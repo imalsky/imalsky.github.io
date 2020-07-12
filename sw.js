39a9a5db66d11ed8f3864b71c5438cb511d39cbc
imalsky <isaacmalsky@gmail.com>
2020-07-12 14:33:43 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5678b2d0d379589d214b.js"
+    "url": "webpack-runtime-3b9ab1ed2736b0b580d6.js"
   },
   {
     "url": "framework-5fc6169171e885c3b522.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9bfae486908794b4635cf6070c0b3f49"
+    "revision": "71428dffa78c0fd3d98e1b0b62ee8279"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
